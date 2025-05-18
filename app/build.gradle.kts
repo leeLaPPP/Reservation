716a8b526d5b6d7696ce6bd14634279f5b92a717
leeLaPPP <zokeoz12@nate.com>
2025-05-19 04:19:06 +0900

    first version

---
+++ app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.date_n_clock_0515"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.date_n_clock_0515"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
---
+++ app/src/main/java/com/example/date_n_clock_0515/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.date_n_clock_0515;
+
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.DatePicker;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Chronometer chronometer;
+    private Button btnStart, btnFinish;
+    private RadioGroup radioGroup;
+    private RadioButton radioDate, radioTime;
+    private TimePicker timePicker;
+    private DatePicker datePicker;
+    private TextView year, month, day, hour, minute;
+    
+    // 선택된 날짜를 저장할 변수들
+    private int selectYear, selectMonth, selectDay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStart);
+        btnFinish = findViewById(R.id.btnFinish);
+        radioGroup = findViewById(R.id.radioGroup);
+        radioDate = findViewById(R.id.radioDate);
+        radioTime = findViewById(R.id.radioTime);
+        timePicker = findViewById(R.id.timePicker);
+        datePicker = findViewById(R.id.datePicker);
+        year = findViewById(R.id.year);
+        month = findViewById(R.id.month);
+        day = findViewById(R.id.day);
+        hour = findViewById(R.id.hour);
+        minute = findViewById(R.id.minute);
+        chronometer = findViewById(R.id.chronometer1);
+
+        timePicker.setVisibility(View.INVISIBLE);
+        datePicker.setVisibility(View.INVISIBLE);
+
+        radioTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePicker.setVisibility(View.VISIBLE);
+                datePicker.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        radioDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timePicker.setVisibility(View.INVISIBLE);
+                datePicker.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.setBase(SystemClock.elapsedRealtime());
+                chronometer.start();
+            }
+        });
+
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chronometer.stop();
+                year.setText(Integer.toString(selectYear));
+                month.setText(Integer.toString(selectMonth));
+                day.setText(Integer.toString(selectDay));
+                hour.setText(Integer.toString(timePicker.getHour()));
+                minute.setText(Integer.toString(timePicker.getMinute()));
+            }
+        });
+
+        datePicker.setOnDateChangedListener(new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int month, int day) {
+                selectYear = year;
+                selectMonth = month + 1;
+                selectDay = day;
+            }
+        });
+
+
+
+    }
+}
